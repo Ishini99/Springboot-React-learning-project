4493a0f07d94ab0649746d2ff0d2d5c1617f0dde
Ishini99 <92759642+Ishini99@users.noreply.github.com>
2024-05-01 15:31:27 +0530

    setup frontend

---
+++ springbootReactDemo/frontend/demo-client/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+     <h2>
+      welcome to frontend
+     </h2>
+     
+    </div>
+  );
+}
+
+export default App;
